3563c8c7fb2b077c7f21efb087fd94488d6b0fd4
George El-jaji <george.el-jaji@exopen.se>
2017-02-23 14:19:33 +0100

    Added functionality to be able to list and call selected functions from console.

---
+++ Available.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CSharp7FeaturesDemo
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class Available: Attribute
+    {}
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
 
 namespace CSharp7FeaturesDemo
 {
     class Program
     {
+        private static readonly Dictionary<int, MethodInfo> AvailableMethods = new Dictionary<int, MethodInfo>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FindAllAvailableMethods();
+
+            while (true)
+            {
+                ListAllAvailableMethodsToCall();
+                var indexOfMethodToInvoke = Console.ReadLine();
+                var parsedIndex = int.Parse(indexOfMethodToInvoke);
+                InvokeMethod(parsedIndex);
+
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        private static void FindAllAvailableMethods()
+        {
+            var programType = typeof(Program);
+            var methodsInClass = programType.GetMethods(BindingFlags.NonPublic | BindingFlags.Static);
+
+            var index = 0;
+            foreach (var method in methodsInClass)
+            {
+                if (method.CustomAttributes.Any(customAttributes => customAttributes.AttributeType == typeof(Available)))
+                {
+                    AvailableMethods.Add(index, method);
+                    index++;
+                }
+            }
+        }
+
+        private static void ListAllAvailableMethodsToCall()
+        {
+            Console.WriteLine("Available methods to call:");
+
+            foreach (var method in AvailableMethods)
+            {
+                Console.WriteLine($"{method.Key}: {method.Value.Name}");
+            }
+
+            Console.WriteLine("Write the number of the method you would like to envoke and press enter:");
+        }
+
+        private static void InvokeMethod(int index)
+        {
+            var methodToInvoke = AvailableMethods[index];
+            Console.WriteLine($"Invoking method: {methodToInvoke.Name}");
+            Thread.Sleep(1000);
+            methodToInvoke.Invoke(null, null);
         }
     }
 }
