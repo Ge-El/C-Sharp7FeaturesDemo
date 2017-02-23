f8d1c4e425149285b80dececc80378bc0566dc30
George El-jaji <george.el-jaji@exopen.se>
2017-02-23 14:10:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp7FeaturesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
