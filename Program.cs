2cea0e2d0ed482551a560fa05a27442c9413bc25
George El-jaji <george.el-jaji@exopen.se>
2017-02-23 16:13:19 +0100

    Started implementing out variables and pattern matching demo.

--- Program.cs
+++ Program.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
 namespace CSharp7FeaturesDemo
 {
     class Program
     {
+        #region Demo Implemenation
         private static readonly Dictionary<int, MethodInfo> AvailableMethods = new Dictionary<int, MethodInfo>();
+        private const int ConsoleWaitTime = 500;
 
         static void Main(string[] args)
         {
@@ -21,7 +24,7 @@ namespace CSharp7FeaturesDemo
                 var parsedIndex = int.Parse(indexOfMethodToInvoke);
                 InvokeMethod(parsedIndex);
 
-                Thread.Sleep(1000);
+                Thread.Sleep(ConsoleWaitTime);
                 Console.WriteLine();
                 Console.WriteLine();
             }
@@ -42,7 +45,6 @@ namespace CSharp7FeaturesDemo
                 }
             }
         }
-
         private static void ListAllAvailableMethodsToCall()
         {
             Console.WriteLine("Available methods to call:");
@@ -59,8 +61,84 @@ namespace CSharp7FeaturesDemo
         {
             var methodToInvoke = AvailableMethods[index];
             Console.WriteLine($"Invoking method: {methodToInvoke.Name}");
-            Thread.Sleep(1000);
+            Thread.Sleep(ConsoleWaitTime);
             methodToInvoke.Invoke(null, null);
         }
+        #endregion
+
+        #region Out Variables
+        /// <summary>
+        /// Tries to parse a <c>Guid</c> and print out result
+        /// </summary>
+        ///<remarks>
+        /// Uses predeclared out variables 
+        /// </remarks>
+        [Available]
+        private static void ParseGuidOld()
+        {
+            Guid parsedGuid;
+            var pareseSucceded = Guid.TryParse("49C49483-A2E8-43B3-B5F5-E5650E35EEF2", out parsedGuid);
+
+            var resultMessage = pareseSucceded ? $"Parse succeded guid: {parsedGuid:D} " : "Parse failed";
+
+            Console.WriteLine(resultMessage);
+        }
+
+
+        /// <summary>
+        /// Tries to parse a <c>Guid</c> and print out result
+        /// </summary>
+        /// <remarks>
+        /// Uses C# 7 feature out variables, declaration of variables is right in the parameter list where they are passed
+        /// </remarks>
+        [Available]
+        private static void ParseGuidNew()
+        {
+            var pareseSucceded = Guid.TryParse("49C49483-A2E8-43B3-B5F5-E5650E35EEF2", out var parsedGuid);
+
+            var resultMessage = pareseSucceded ? $"Parse succeded guid: {parsedGuid:D} " : "Parse failed";
+
+            Console.WriteLine(resultMessage);
+        }
+
+
+        /// <summary>
+        /// Prints out the value of x
+        /// </summary>
+        /// <remarks>
+        /// Uses C# 7 feature out variables declared in the parameterlist and wildcard.
+        /// </remarks>
+        [Available]
+        private static void PrintPoint()
+        {
+            // GetPoints(out var x, out *); // I only care about x
+
+            // Console.WriteLine($"X: {x}");
+
+        }
+
+        [Available]
+        private static void GetPoints(out int x, out int y)
+        {
+            x = 1;
+            y = 2;
+        }
+
+        #endregion
+
+        #region Pattern Matching
+
+        [Available]
+        private static void Old()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Available]
+        private static void New()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
