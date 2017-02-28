852c17f98fe7dcbc8e1186ff97a38a955c735d15
George El-jaji <george.el-jaji@exopen.se>
2017-02-28 10:30:16 +0100

    Added demo for binary literals and pattern matching.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Net.Sockets;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
@@ -66,6 +68,122 @@ namespace CSharp7FeaturesDemo
         }
         #endregion
 
+        #region Binary Literals
+        [Available]
+        static void BinaryLiteralsOld()
+        {
+            int[] numbers = { 1, 2, 4, 8, 16, 32 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        [Available]
+        static void BinaryLiteralsNew()
+        {
+            int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b10000, 0b100000 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        [Available]
+        static void BinaryLiteralsNewWithDigitSeperator()
+        {
+            int[] numbers = { 0b1, 0b10, 0b100, 0b1_000, 0b10_000, 0b100_000 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        #endregion
+
+        #region Tuples / Deconstruction / Local Functions
+        static readonly int[] Numbers = { 0b1, 0b10, 0b100, 0b1_000, 0b10_000, 0b100_000 };
+
+        [Available]
+        static void TuplesOld()
+        {
+            var result = Tally(Numbers);
+
+            Console.WriteLine( $" Count:{result.Item1}. result:{result.Item2}");
+        }
+
+        static Tuple<int, int> Tally(int[] values)
+        {
+            var sum = 0;
+            var count = 0;
+
+            foreach (var value in values)
+            {
+                sum += value;
+                count++;
+            }
+
+            return new Tuple<int, int>(count, sum);
+        }
+
+        [Available]
+        static void TuplesNew()
+        {
+            var result = TallyNew(Numbers);
+
+            Console.WriteLine($" Count:{result.count}. result:{result.sum}");
+        }
+
+        static (int count, int sum) TallyNew(int[] values)
+        {
+            var result = (count: 0, sum: 0);
+
+            foreach (var value in values)
+            {
+                result.sum += value;
+                result.count++;
+            }
+
+            return result;
+        }
+
+        [Available]
+        static void TuplesNewDeconstruction()
+        {
+             (var count, var sum) = TallyNew(Numbers);
+
+            Console.WriteLine($" Count:{count}. result:{sum}.");
+        }
+
+        [Available]
+        static void TuplesNewLocalFunction()
+        {
+            (var count, var sum) = TallyNewLocalFunction(Numbers);
+
+            Console.WriteLine($" Count:{count}. result:{sum}.");
+        }
+
+        static (int count, int sum) TallyNewLocalFunction(int[] numbers)
+        {
+            var result = (count: 0, sum: 0);
+
+            void Add(int sum, int count)
+            {
+                result.sum += sum;
+                result.count += count;
+            }
+
+            foreach (var number in numbers)
+            {
+                Add(number, 1);
+            }
+            return result;
+        }
+
+        #endregion
+
         #region Out Variables
         /// <summary>
         /// Tries to parse a <c>Guid</c> and print out result
@@ -84,7 +202,6 @@ namespace CSharp7FeaturesDemo
             Console.WriteLine(resultMessage);
         }
 
-
         /// <summary>
         /// Tries to parse a <c>Guid</c> and print out result
         /// </summary>
@@ -100,7 +217,6 @@ namespace CSharp7FeaturesDemo
 
             Console.WriteLine(resultMessage);
         }
-
 
         /// <summary>
         /// Prints out the value of x
@@ -123,21 +239,119 @@ namespace CSharp7FeaturesDemo
             x = 1;
             y = 2;
         }
-
         #endregion
 
         #region Pattern Matching
-
         [Available]
-        private static void Old()
+        private static void OldPatternMatching()
         {
-            throw new NotImplementedException();
+            var obj = new object();
+
+            if (obj == null) return;
+            if (obj is int) return;
+
+            Console.WriteLine($"'*'{obj}");
         }
 
         [Available]
-        private static void New()
+        private static void NewPatternMatching()
         {
-            throw new NotImplementedException();
+            var obj = new object();
+            if (obj is null) Console.WriteLine("obj is null"); // Constant pattern
+            if (!(obj is int i)) return; // Type pattern
+
+            Console.WriteLine($"'*'{i}");
+        }
+
+        class Person
+        {
+            public string Firstname { get; set; }
+        }
+
+        class Student: Person
+        {
+            public decimal GradePointAverage { get; set; }
+        }
+
+         class Teacher: Person
+        {
+            public string MainSubject { get; set; }
+        }
+
+        static Teacher _teacher = new Teacher
+        {
+            Firstname = "Bill",
+            MainSubject = "Math"
+        };
+
+        static Student _goodStudent = new Student
+        {
+            Firstname = "Erik",
+            GradePointAverage = 4.8m
+        };
+
+        static  Student _badStudent = new Student
+        {
+            Firstname = "Sarah",
+            GradePointAverage = 2.3m
+        };
+
+        [Available]
+        private static void SwitchStatmentWithPatternMatchingOld()
+        {
+            CommentPersonOld(_teacher);
+            CommentPersonOld(_goodStudent);
+            CommentPersonOld(_badStudent);
+       
+        }
+
+        [Available]
+        private static void SwitchStatmentWithPatternMatchingNew()
+        {
+            CommentPersonNew(_teacher);
+            CommentPersonNew(_goodStudent);
+            CommentPersonNew(_badStudent);
+        }
+
+        static void CommentPersonOld(Person person)
+        {
+            if (person is Student && ((Student)person).GradePointAverage >= 4m)
+            {
+                Console.WriteLine($"{person.Firstname} is a good student");
+            }
+            else if (person is Student && ((Student)person).GradePointAverage < 4m)
+            {
+                Console.WriteLine($"{person.Firstname} is not a good student");
+            }
+            else if (person is Teacher)
+            {
+                Console.WriteLine($"{person.Firstname} main subject is {((Teacher)person).MainSubject}");
+            }
+            else
+            {
+                Console.WriteLine($"Persons name is {person.Firstname}");
+            }
+        }
+
+        static void CommentPersonNew(Person person)
+        {
+            switch (person)
+            {
+                case Student gs when (gs.GradePointAverage >= 4m):
+                    Console.WriteLine($"{gs.Firstname} is a good student");
+                    break;
+                case Student bs when (bs.GradePointAverage < 4m):
+                    Console.WriteLine($"{bs.Firstname} is not a good student");
+                    break;
+                case Teacher t:
+                    Console.WriteLine($"{t.Firstname} main subject is {t.MainSubject}");
+                    break;
+                default:
+                    Console.WriteLine($"Persons name is {person.Firstname}");
+                    break;
+                case null:
+                    throw new ArgumentNullException(nameof(person));
+            }
         }
         #endregion
     }
